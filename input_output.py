84e278a417a10c9360f522c01ec320697bb68ac9
muhamadirpan266 <fotopribadi56@gmail.com>
2026-03-13 09:33:48 +0700

    update

--- input_output.py
+++ input_output.py
@@ -8,3 +8,27 @@ print("2", end ="")
 print("3")
 
 
+#separator
+print("Andi", "budi", "robi", sep="-")
+
+
+
+#penjumlahan
+num1 = input("isikan bilangan 1 :")
+num2 = input("isikan bilangan 2 :")
+result = int(num1) + int(num2)
+
+print(num1, "+", num2, "=", result)
+print("{} + {} = {}".format(num1, num2, result))
+
+f_name = input("isikan nama awal :")
+m_name = input("isikan nama tengah :")
+l_name = input("isikan nama akhir :")
+
+print("nama anda : {} {} {}".format(f_name, m_name, l_name))
+
+#index
+print("nama anda : {1} {0} {1}".format(f_name, m_name, l_name))
+
+#keyword
+print("nama anda : {depan} {tengah} {belakang}".format(depan = f_name, tengah = m_name, belakang = l_name))
